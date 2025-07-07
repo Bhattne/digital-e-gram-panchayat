9323019d79dd73345f266e2ddc22f209148d63cd
Niharika Bhatt <niharikabha00@>
2025-07-07 14:13:22 +0530

    First commit

--- tilt.js
+++ tilt.js
@@ -1,7 +1,17 @@
-// Initialize VanillaTilt
-VanillaTilt.init(document.querySelectorAll(".tilt"), {
-  max: 10,
-  speed: 400,
-  glare: true,
-  "max-glare": 0.2
+// Wait for DOM content to load
+window.addEventListener("DOMContentLoaded", function () {
+  // Initialize VanillaTilt
+  VanillaTilt.init(document.querySelectorAll(".tilt"), {
+    max: 15,
+    speed: 400,
+    scale: 1.05,
+    glare: true,
+    "max-glare": 0.2
+  });
+
+  // Initialize AOS
+  AOS.init({
+    duration: 1000,
+    once: true
+  });
 });
